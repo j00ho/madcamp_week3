6a32d39f9b7a962987006e197911af9505f0468b
surface03 <ginigini03@gmail.com>
2024-01-12 16:41:08 +0900

    added router

---
+++ myapp/src/B.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const B = () => {
+	return (
+		<>
+			<h3>안녕하세요. B 입니다.</h3>
+		</>
+	);
+};
+
+export default B;
---
+++ myapp/src/D.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const D = () => {
+	return (
+		<>
+			<h3>안녕하세요. D 입니다.</h3>
+		</>
+	);
+};
+
+export default D;
