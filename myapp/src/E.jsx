59a161e40f67bd5378ae8b2bd74b1b3c87e5957a
surface03 <ginigini03@gmail.com>
2024-01-12 17:02:53 +0900

    add video

--- myapp/src/E.jsx
+++ myapp/src/E.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+const YouTubeVideo = ({ videoId }) => {
+  const videoSrc = `https://www.youtube.com/embed/${videoId}`;
+
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src={videoSrc}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  );
+};
 
 const E = () => {
-	return (
-		<>
-			<h3>안녕하세요. E 입니다.</h3>
-		</>
-	);
+  return (
+    <div>
+      <h1>YouTube Video Example</h1>
+      <YouTubeVideo videoId="3LILV0dK9ys" /> {/* 여기에 원하는 YouTube 비디오 ID를 넣습니다. */}
+    </div>
+  );
 };
 
 export default E;
