baa3c4e9d1e6aea0ace4c6ba1374450919b38094
JuhoSong <qhdrhf5144@kaist.ac.kr>
2024-01-17 00:10:44 +0900

    Update RegisterPage.jsx

--- myapp/src/pages/RegisterPage.jsx
+++ myapp/src/pages/RegisterPage.jsx
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import AuthTemplate from "../components/auth/AuthTemplate";
 import AuthForm from "../components/auth/AuthForm";
 
 const RegisterPage = () => {
-    return (
-        <AuthTemplate>
-            <AuthForm type="register"/>
-        </AuthTemplate>
-    );
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    gender: "",
+    campus: "",
+    income: "",
+    age: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // 서버로 POST 요청 보내기
+      // 여기에 필요한 API 호출 코드를 작성하세요
+    } catch (error) {
+      // 오류 처리
+      console.error("회원가입 실패:", error.response.data.error);
+    }
+  };
+
+  return (
+    <AuthTemplate>
+      <AuthForm
+        type="register"
+        formData={formData}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+    </AuthTemplate>
+  );
 };
 
 export default RegisterPage;
