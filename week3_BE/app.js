c6d9b0c013ba02c012c71ac87491ccd7d0ef70b8
JuhoSong <qhdrhf5144@kaist.ac.kr>
2024-01-16 23:54:11 +0900

    Update app.js

--- week3_BE/app.js
+++ week3_BE/app.js
@@ -7,9 +7,10 @@ const session = require('express-session');
 const Filestore = require('session-file-store')(session);
 
 const app = express()
+const cors = require('cors')
 const port = 3000
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json()); 
 
@@ -21,6 +22,11 @@ app.use(session({
   store: new Filestore()
 }));
 
+app.use(cors());
+// app.use(cors({
+//   origin: 'http://localhost:5173', // 클라이언트의 주소
+//   credentials: true,
+// }));
 app.use('/payment', paymentRoutes);
 app.use('/user', userRoutes);
 
