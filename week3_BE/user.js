13cba6f1b18e79833e374ed127ceabe7479ab7c4
surface03 <ginigini03@gmail.com>
2024-01-17 11:14:40 +0000

    지출 정보 조회까지 완료

--- week3_BE/user.js
+++ week3_BE/user.js
@@ -12,6 +12,7 @@ router.use(cors({
 }));
 
 // router.use(cors());
+const jwt = require('jsonwebtoken');
 
 // 세션 설정
 router.use(session({
@@ -72,10 +73,12 @@ router.post('/login', async (req, res) => {
 
     if (user.length > 0) {
       // 로그인 성공
+      const token = jwt.sign({ useremail: user[0].email }, 'your-secret-key', { expiresIn: '1h' });
       // 세션에 user_id 저장
       req.session.user = user[0];
       // console.log(req.session.user);
-      res.json({ message: '로그인 성공', user: user[0].username });
+      res.json({ message: '로그인 성공', user: user[0].username, authToken: token, userId: user[0].user_id });
+      console.log(token);
     } else {
       // 로그인 실패
       res.status(401).json({ error: '이메일 또는 비밀번호가 일치하지 않습니다.' });
