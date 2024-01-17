00d715b1892ba84e12e3bc2c88d957bc3180d235
surface03 <ginigini03@gmail.com>
2024-01-17 20:22:07 +0900

    added example graph

--- myapp/src/MyGraphPage.jsx
+++ myapp/src/MyGraphPage.jsx
@@ -10,23 +10,231 @@ import Box from '@mui/material/Box';
 const openai = new OpenAI({apiKey: import.meta.env.VITE_OPENAI_API_KEY, dangerouslyAllowBrowser: true});
 
 const MyGraphPage = () => {
+  const datasets = [
+    // datasets[0]
+    [
+      {
+        name: '식비',
+        data: [31500, 0, 2500, 31000, 0, 0],
+      },
+      {
+        name: '교통',
+        data: [0, 20000, 0, 0, 0, 0],
+      },
+      {
+        name: '쇼핑',
+        data: [2500, 0, 31500, 0, 0, 0],
+      },
+      {
+        name: '연애',
+        data: [31000, 0, 0, 61000, 0, 0],
+      },
+      {
+        name: '교육',
+        data: [0, 0, 0, 0, 25200, 0],
+      },
+      {
+        name: '기타',
+        data: [0, 0, 0, 0, 0, 70010],
+      },
+    ],
+
+    // datasets[1]
+    [
+      {
+        name: '식비',
+        data: [65000, 0, 0, 0, 0, 0],
+      },
+      {
+        name: '교통',
+        data: [0, 3000, 1000, 16000, 0, 0],
+      },
+      {
+        name: '쇼핑',
+        data: [0, 1000, 34000, 0, 0, 0],
+      },
+      {
+        name: '연애',
+        data: [0, 16000, 0, 76000, 0, 0],
+      },
+      {
+        name: '교육',
+        data: [0, 0, 0, 0, 25200, 0],
+      },
+      {
+        name: '기타',
+        data: [0, 0, 0, 0, 0, 70010],
+      },
+    ],
+
+    // datasets[2]
+    [
+      {
+        name: '식비',
+        data: [62500, 0, 2500, 0, 0, 0],
+      },
+      {
+        name: '교통',
+        data: [0, 17000, 3000, 0, 0, 0],
+      },
+      {
+        name: '쇼핑',
+        data: [2500, 3000, 13500, 15000, 0, 0],
+      },
+      {
+        name: '연애',
+        data: [0, 0, 15000, 77000, 0, 0],
+      },
+      {
+        name: '교육',
+        data: [0, 0, 0, 0, 25200, 0],
+      },
+      {
+        name: '기타',
+        data: [0, 0, 0, 0, 0, 70010],
+      },
+    ],
+
+    // datasets[3]
+    [
+      {
+        name: '식비',
+        data: [65000, 0, 0, 0, 0, 0],
+      },
+      {
+        name: '교통',
+        data: [0, 20000, 0, 0, 0, 0],
+      },
+      {
+        name: '쇼핑',
+        data: [0, 0, 34000, 0, 0, 0],
+      },
+      {
+        name: '연애',
+        data: [0, 0, 0, 92000, 0, 0],
+      },
+      {
+        name: '교육',
+        data: [0, 0, 0, 0, 25200, 0],
+      },
+      {
+        name: '기타',
+        data: [0, 0, 0, 0, 0, 70010],
+      },
+    ],
+
+    // datasets[4]
+    [
+      {
+        name: '식비',
+        data: [65000, 0, 0, 0, 0, 0],
+      },
+      {
+        name: '교통',
+        data: [0, 20000, 0, 0, 0, 0],
+      },
+      {
+        name: '쇼핑',
+        data: [0, 0, 34000, 0, 0, 0],
+      },
+      {
+        name: '연애',
+        data: [0, 0, 0, 92000, 0, 0],
+      },
+      {
+        name: '교육',
+        data: [0, 0, 0, 0, 25200, 0],
+      },
+      {
+        name: '기타',
+        data: [0, 0, 0, 0, 0, 70010],
+      },
+    ],
+
+    // datasets[5]
+    [
+      {
+        name: '식비',
+        data: [65000, 0, 0, 0, 0, 0],
+      },
+      {
+        name: '교통',
+        data: [0, 20000, 0, 0, 0, 0],
+      },
+      {
+        name: '쇼핑',
+        data: [0, 0, 34000, 0, 0, 0],
+      },
+      {
+        name: '연애',
+        data: [0, 0, 0, 92000, 0, 0],
+      },
+      {
+        name: '교육',
+        data: [0, 0, 0, 0, 25200, 0],
+      },
+      {
+        name: '기타',
+        data: [0, 0, 0, 0, 0, 70010],
+      },
+    ],
+
+    // datasets[6]
+    [
+      {
+        name: '식비',
+        data: [65000, 0, 0, 0, 0, 0],
+      },
+      {
+        name: '교통',
+        data: [0, 20000, 0, 0, 0, 0],
+      },
+      {
+        name: '쇼핑',
+        data: [0, 0, 34000, 0, 0, 0],
+      },
+      {
+        name: '연애',
+        data: [0, 0, 0, 92000, 0, 0],
+      },
+      {
+        name: '교육',
+        data: [0, 0, 0, 0, 25200, 0],
+      },
+      {
+        name: '기타',
+        data: [0, 0, 0, 0, 0, 70010],
+      },
+    ],
+  ];
+
+  const stackedChartOptions = {
+    chart: {
+      type: 'bar',
+      stacked: true,
+    },
+    xaxis: {
+      categories: ['식비', '교통', '쇼핑', '연애', '교육', '기타'],
+    }
+  };
+
   // 상단 그래프 데이터
   const dailyData = {
-    series: [{ name: '일별 소비', data: [10, 50, 30, 40, 60, 20, 70] }],
+    series: [{ name: '일별 소비', data: [10100, 52340, 35200, 12000, 1200, 42000, 18200] }],
     options: {
       chart: { type: 'line' },
       xaxis: { categories: ['월', '화', '수', '목', '금', '토', '일'] },
     },
   };
   const weeklyData = {
-    series: [{ name: '주간별 소비', data: [100, 200, 300, 400, 500] }],
+    series: [{ name: '주간별 소비', data: [130000, 19000, 23300, 4400, 55500] }],
     options: {
       chart: { type: 'bar' },
       xaxis: { categories: ['1주', '2주', '3주', '4주', '5주'] },
     },
   };
   const monthlyData = {
-    series: [{ name: '월별 소비', data: [1000, 2000, 3000, 4000, 5000] }],
+    series: [{ name: '월별 소비', data: [704000, 201100, 300430, 403500, 500300] }],
     options: {
       chart: { type: 'bar' },
       xaxis: { categories: ['1월', '2월', '3월', '4월', '5월'] },
@@ -36,16 +244,15 @@ const MyGraphPage = () => {
   // Stacked Chart 데이터
   const [selectedTag, setSelectedTag] = useState('');
   const [chartData, setChartData] = useState([]);
-  const tags = ['식비', '교통비', '기타']; // 가정된 태그 목록
+  const tags = ['식비', '교통', '쇼핑', '연애', '교육', '기타', '태그 선택 안함']; // 가정된 태그 목록
 
   useEffect(() => {
-    // 선택된 태그에 따라 그래프 데이터 업데이트 로직
-    if (selectedTag === '식비') {
-      setChartData([44, 55, 41, 67, 22, 43]);
-    } else if (selectedTag === '교통비') {
-      setChartData([13, 23, 20, 8, 13, 27]);
-    } else if (selectedTag === '기타') {
-      setChartData([11, 17, 15, 15, 21, 14]);
+    // Update chartData based on selectedTag
+    const index = tags.indexOf(selectedTag);
+    if (index !== -1) {
+      setChartData(datasets[index]);
+    } else {
+      setChartData([]);
     }
   }, [selectedTag]);
 
@@ -105,8 +312,8 @@ const MyGraphPage = () => {
       </Select>
 
       <Chart
-        options={{ chart: { type: 'bar' }, xaxis: { categories: tags } }}
-        series={[{ name: selectedTag, data: chartData }]}
+        options={stackedChartOptions}
+        series={chartData}
         type="bar"
       />
 
